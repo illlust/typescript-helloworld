c451494f221e87d270ded2d575441fe49387732f
mike <illlust@gmail.com>
2017-11-02 00:06:43 +0900

    test typescript-rest

---
+++ index.ts
@@ -0,0 +1,25 @@
+import * as express from "express";
+import { Server, Path, GET, PathParam } from "typescript-rest";
+
+@Path("/hello")
+class HelloService {
+    @Path(":name")
+    @GET
+    sayHello( @PathParam('name') name: string): string {
+        // return "Hello " + name;
+        // return JSON.parse('{"hello": "world"}');
+        let jsonString = JSON.stringify({ name: name, a: "한글", b: "eng", c: true, d: { e: 1, f: [1, 2] } }, null, 4); // Indented 4 spaces
+        // return jsonString;
+        return JSON.parse(jsonString);
+    }
+}
+
+let app: express.Application = express();
+Server.buildServices(app);
+
+app.listen(3000, function () {
+    console.log('Rest Server listening on port 3000!');
+});
+
+// REQ http://localhost:3000/hello/world
+// RES {"name":"world","a":"한글","b":"eng","c":true,"d":{"e":1,"f":[1,2]}}
