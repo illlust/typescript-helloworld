31703ed545bca1118fa64cba1bced2ec8a8fcc7d
mike <illlust@gmail.com>
2017-11-01 23:33:03 +0900

    first commit

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+var Startup = /** @class */ (function () {
+    function Startup() {
+    }
+    Startup.main = function () {
+        console.log('Hello World');
+        return 0;
+    };
+    return Startup;
+}());
+Startup.main();
+// run node helloworld.js on terminal 
+//# sourceMappingURL=helloworld.js.map
